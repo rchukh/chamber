bffc9bbe098cd04edd796491ba72d38cc609e850
Roman Chukh <roman.chukh@gmail.com>
2014-04-20 21:01:43 +0300

    feat(tools): add berkshelf support
    Add http://berkshelf.com dependency manager.
    
    Closes #2

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'chamber'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chamber'
+long_description 'Installs/Configures chamber'
+version          '0.0.1'
+

3c692bfb822fa334afdcbb2ed8c6b38c65e1fba5
Roman Chukh <roman.chukh@gmail.com>
2014-04-21 22:39:29 +0300

    refactor: fix rubocop violations

--- recipes/jdk.rb
+++ recipes/jdk.rb
@@ -3,12 +3,12 @@
 # Recipe:: jdk
 #
 # Workaround for Oracle which is not supporting normal download from website
-cookbook_file "/tmp/jdk-7u55-linux-x64.tar.gz" do
-  source "vendor/jdk-7u55-linux-x64.tar.gz"
+cookbook_file '/tmp/jdk-7u55-linux-x64.tar.gz' do
+  source 'vendor/jdk-7u55-linux-x64.tar.gz'
   mode 0644
 end
-cookbook_file "/tmp/UnlimitedJCEPolicyJDK7.zip" do
-  source "vendor/UnlimitedJCEPolicyJDK7.zip"
+cookbook_file '/tmp/UnlimitedJCEPolicyJDK7.zip' do
+  source 'vendor/UnlimitedJCEPolicyJDK7.zip'
   mode 0644
 end
 
--- recipes/maven.rb
+++ recipes/maven.rb
@@ -4,6 +4,6 @@
 #
 node.set['maven']['version'] = 3
 
-# Make sure included recipes are using correct values in attributes that are reusing the ones defined above
+# Make sure included recipes are using correct values in attributes which are reusing the ones defined above
 node.from_file(run_context.resolve_attribute('maven', 'default'))
 include_recipe 'maven::default'
